86bb4f73a4eddf2ebf2b73ba44c0906129877e72
MiHii <hieupham08111@gmail.com>
2025-01-02 16:00:59 +0700

    debug

--- src/lib/analytics.tsx
+++ src/lib/analytics.tsx
@@ -1,12 +1,17 @@
-"use client";
+'use client';
 
 import dynamic from 'next/dynamic';
+console.log(
+  'process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID',
+  process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+);
 
-const GoogleAnalytics = dynamic(
-    () => import("@next/third-parties/google")
-        .then(mod => mod.GoogleAnalytics)
+const GoogleAnalytics = dynamic(() =>
+  import('@next/third-parties/google').then((mod) => mod.GoogleAnalytics),
 );
 
 export const Analytics = () => {
-    return <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!} />
-}
+  return (
+    <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!} />
+  );
+};
