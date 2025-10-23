fc4b7672cd860ed5e5715c9fe076eb9b2a8e07d8
MiHii <hieupham08111@gmail.com>
2025-10-23 22:58:36 +0700

    fix: update metadata description to reflect current role and experience

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -4,7 +4,7 @@ import { Metadata } from 'next';
 
 export function constructMetadata({
   title = 'Pham Minh Hieu', // TODO: Add a custom title
-  description = 'I am a student passionate about learning web development and creating user-friendly, accessible web applications.', // TODO: Add a custom description
+  description = 'Bridge Software Engineer with a background in web development and five years of study in Japan.', // TODO: Add a custom description
   image = '/thumbnail.png', // TODO: Add a custom image
   icons = '/logo.png', // TODO: Add a custom icon
   noIndex = false,
